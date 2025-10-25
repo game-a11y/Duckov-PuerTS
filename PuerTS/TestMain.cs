85abc601b59f914b82351c70afbcade12ade7c35
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-25 18:17:15 +0800

    Add PuerTS_Core_3.0.0-preview0

---
+++ PuerTS/TestMain.cs
@@ -0,0 +1,20 @@
+﻿using Puerts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Puerts;
+
+namespace PuerTS
+{
+    internal class TestMain
+    {
+        public static void Main()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            jsEnv.Eval(@"
+                CS.System.Console.WriteLine('hello world');
+            ");
+            jsEnv.Dispose();
+        }
+    }
+}
