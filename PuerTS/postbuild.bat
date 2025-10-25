e6f9a65147c982b26e7866f86f395872dd110f91
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-25 21:34:27 +0800

    复制构建产物到 mod 文件夹

--- PuerTS/postbuild.bat
+++ PuerTS/postbuild.bat
@@ -1,13 +1,20 @@
 echo Copying Assets to Release Dir
 
-@REM Runtime files
-xcopy %1\upm\    %1\PuerTS\upm\  /E /Y
+@REM Copy Runtime files
+@REM xcopy %1\upm\    %1\PuerTS\upm\  /E /Y
+xcopy %1\upm\Editor\Resources\      %1\PuerTS\upm\Editor\Resources\  /E /Y
+xcopy %1\upm\Runtime\Resources\     %1\PuerTS\upm\Runtime\Resources\  /E /Y
+copy %1\upm\LICENSE         %1\PuerTS\upm\
+copy %1\upm\package.json    %1\PuerTS\upm\
+copy %1\upm\Plugins\x86_64\*.dll    %1\PuerTS\
 
-@REM DLLs
-copy %1\upm\Plugins\x86_64\PapiV8.dll       %1\PuerTS\
-copy %1\upm\Plugins\x86_64\PuertsCore.dll   %1\PuerTS\
-
-@REM copy mod
+@REM Copy MOD dll
 copy %2\PuerTS.dll  %1\PuerTS\
 copy %2\PuerTS.pdb  %1\PuerTS\
 copy %2\PuerTS.deps.json    %1\PuerTS\
+
+@REM Copy mod meta files
+copy %1\info.ini        %1\PuerTS\
+copy %1\preview.png     %1\PuerTS\
+copy %1\..\LICENSE      %1\PuerTS\
+copy %1\..\README.md    %1\PuerTS\
