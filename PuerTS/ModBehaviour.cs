807b7adeaeece540e32fcb0429faa5d0590ac743
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-25 22:21:57 +0800

    删除示例代码

--- PuerTS/ModBehaviour.cs
+++ PuerTS/ModBehaviour.cs
@@ -9,66 +9,38 @@ using UnityEngine.UI;
 
 namespace PuerTS
 {
-
     public class ModBehaviour : Duckov.Modding.ModBehaviour
     {
-        TextMeshProUGUI _text = null;
-        TextMeshProUGUI Text
-        {
-            get
-            {
-                if (_text == null)
-                {
-                    _text = Instantiate(GameplayDataSettings.UIStyle.TemplateTextUGUI);
-                }
-                return _text;
-            }
-        }
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        const string MOD_NAME = "PuerTS";
 
         void Awake()
         {
-            Debug.Log("PuerTS Loaded!!!");
+            Debug.Log($"[{MOD_NAME}]: MOD Start.");
 
-            Debug.Log("PuerTS: JsEnv Start.");
+            Debug.Log($"[{MOD_NAME}]: JsEnv init.");
             var jsEnv = new JsEnv(new ResLoader());
             // `Scripts/` 已经在搜索路径中了，无需再添加
             jsEnv.ExecuteModule("index.js");
             jsEnv.Dispose();
         }
+
         void OnDestroy()
         {
-            if (_text != null)
-                Destroy(_text);
+            Debug.Log($"[{MOD_NAME}]: MOD Destroy.");
         }
+
         void OnEnable()
         {
-            ItemHoveringUI.onSetupItem += OnSetupItemHoveringUI;
-            ItemHoveringUI.onSetupMeta += OnSetupMeta;
+            Debug.Log($"[{MOD_NAME}]: MOD Enable.");
         }
+
         void OnDisable()
         {
-            ItemHoveringUI.onSetupItem -= OnSetupItemHoveringUI;
-            ItemHoveringUI.onSetupMeta -= OnSetupMeta;
+            Debug.Log($"[{MOD_NAME}]: MOD Disable.");
         }
 
-        private void OnSetupMeta(ItemHoveringUI uI, ItemMetaData data)
-        {
-            Text.gameObject.SetActive(false);
-        }
-
-        private void OnSetupItemHoveringUI(ItemHoveringUI uiInstance, Item item)
-        {
-            if (item == null)
-            {
-                Text.gameObject.SetActive(false);
-                return;
-            }
-            
-            Text.gameObject.SetActive(true);
-            Text.transform.SetParent(uiInstance.LayoutParent);
-            Text.transform.localScale = Vector3.one;
-            Text.text = $"${item.GetTotalRawValue() / 2}";
-            Text.fontSize = 20f;
-        }
     }
 }
