57a9eca3691b140090e44e7027f0d596b577a127
Chengyu HAN <cyhan.dev@outlook.com>
2026-05-28 18:42:15 +0800

    chore: 发布目录 LICENSE 重命名为带标识的文件名
    - upm/LICENSE → Puerts+V8-LICENSE.txt
    - LICENSE → Duckov-PuerTS-LICENSE.txt

--- PuerTS/postbuild.ps1
+++ PuerTS/postbuild.ps1
@@ -11,7 +11,7 @@ if (Test-Path "$ProjectDir\upm\Editor\Resources") {
 }
 Copy-Item "$ProjectDir\upm\Runtime\Resources\*" "$ProjectDir\PuerTS\upm\Runtime\Resources\" -Recurse -Force
 Copy-Item "$ProjectDir\Scripts\*" "$ProjectDir\PuerTS\Scripts\" -Recurse -Force
-Copy-Item "$ProjectDir\upm\LICENSE" "$ProjectDir\PuerTS\upm\" -Force
+Copy-Item "$ProjectDir\upm\LICENSE" "$ProjectDir\PuerTS\Puerts+V8-LICENSE.txt" -Force
 Copy-Item "$ProjectDir\upm\package.json" "$ProjectDir\PuerTS\upm\" -Force
 Copy-Item "$ProjectDir\upm\Plugins\x86_64\*.dll" "$ProjectDir\PuerTS\" -Force
 
@@ -26,6 +26,6 @@ Copy-Item "$TargetDir\PuerTSRuntime.pdb" "$ProjectDir\PuerTS\" -Force
 $RepoRoot = Join-Path $ProjectDir '..'
 Copy-Item "$ProjectDir\info.ini" "$ProjectDir\PuerTS\" -Force
 Copy-Item "$ProjectDir\preview.png" "$ProjectDir\PuerTS\" -Force
-Copy-Item "$RepoRoot\LICENSE" "$ProjectDir\PuerTS\" -Force
+Copy-Item "$RepoRoot\LICENSE" "$ProjectDir\PuerTS\Duckov-PuerTS-LICENSE.txt" -Force
 Copy-Item "$RepoRoot\README.md" "$ProjectDir\PuerTS\" -Force
 Copy-Item "$RepoRoot\CHANGELOG.md" "$ProjectDir\PuerTS\" -Force
